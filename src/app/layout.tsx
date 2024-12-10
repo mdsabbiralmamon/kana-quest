63402bc0d349e306385c440588783be988efefa8
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-12-10 17:00:55 +0600

    refactor: remove Navbar component from layout for cleaner structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper/SessionProviderWrapper";
-import Navbar from "@/components/global/Navbar/page";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,15 +30,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header>
-            <Navbar />
-          </header>
-          <main>
             {children}
-          </main>
-          <footer>
-
-          </footer>
         </body>
       </SessionProviderWrapper>
     </html>
