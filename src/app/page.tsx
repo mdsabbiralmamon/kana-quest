a9030c81bca2f03b3d596cd5f4efbd3d89a09c51
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-12-10 17:01:05 +0600

    refactor: simplify Home component structure by removing session handling and adding Navbar and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,21 @@
-'use client';
-
+import Footer from '@/components/global/Footer/Footer';
+import Navbar from '@/components/global/Navbar/page';
 import React from 'react';
-import { signOut, useSession } from 'next-auth/react';
-import Image from 'next/image';
 
-const Page = () => {
-  const handleLogout = async () => {
-    signOut();
-  };
-
-  const { data: session } = useSession();
-  console.log(session);
-
+const Home = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        flexDirection: 'column',
-      }}
-    >
-      <h1>Welcome to Your App</h1>
-      <h2>{session?.user?.name}</h2>
-      <h3>{session?.user?.email}</h3>
-      <h4>{session?.user?.role}</h4>
-
-      {/* Conditionally render the image */}
-      {session?.user?.photo ? (
-        <Image src={session.user.photo} width={100} height={100} alt="Profile Picture" />
-      ) : (
-        <Image src="/default-avatar.jpg" width={100} height={100} alt="Default Avatar" />
-      )}
-
-      <button
-        onClick={handleLogout}
-        style={{
-          padding: '10px 20px',
-          backgroundColor: '#0070f3',
-          color: '#fff',
-          border: 'none',
-          borderRadius: '5px',
-          cursor: 'pointer',
-          marginTop: '20px',
-        }}
-      >
-        Logout
-      </button>
-    </div>
+    <>
+    <header>
+      <Navbar />
+    </header>
+    <main>
+      <h1>Home</h1>
+    </main>
+    <footer>
+      <Footer />
+    </footer>
+    </>
   );
 };
 
-export default Page;
+export default Home;
