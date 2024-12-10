a7d19c7fbf61b4eb05fb4e161b2bca748d1f09d1
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-12-10 12:46:37 +0600

    add optional photo property to User, Session, and JWT interfaces in NextAuth types

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -5,12 +5,14 @@ declare module "next-auth" {
   interface User extends DefaultUser {
     id: string;
     role: string; // Add role property
+    photo?: string; // Add photo property (optional)
   }
 
   interface Session {
     user: {
       id: string;
       role: string; // Add role property
+      photo?: string; // Add photo property (optional)
     } & DefaultSession["user"];
   }
 }
@@ -19,5 +21,6 @@ declare module "next-auth/jwt" {
   interface JWT {
     id: string;
     role: string; // Add role property
+    photo?: string; // Add photo property (optional)
   }
 }
