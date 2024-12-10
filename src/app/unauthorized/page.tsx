049dfbb2f1522b3f74af6aff2050bfa953a2a63c
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-12-10 12:46:26 +0600

    add Unauthorized page component to handle access restrictions

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+export default function Unauthorized() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Unauthorized</h1>
+        <p>You do not have permission to access this page.</p>
+      </div>
+    );
+  }
+  
